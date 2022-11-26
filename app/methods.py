f8883e14b2b6ffc53add53629e12ef62971edec0
Delifer <Delifer@mail.com>
2022-11-27 02:14:43 +0300

    V1

---
+++ app/methods.py
@@ -0,0 +1,27 @@
+import sqlite3 as sq
+from sqlite3 import OperationalError
+import sys
+
+class DBases():
+    def connect_to_base(self, namedatabase):
+        try:
+            self.con = sq.connect(namedatabase)
+            self.cur = self.con.cursor()
+        except NameError as err:
+           print("No don't enter the name: ", err)
+           
+
+    def create_table(self, nametable):
+        try:
+            self.cur.execute("CREATE TABLE "+nametable+"(login TEXT,password TEXT)")
+            self.con.commit()
+        except OperationalError as err:
+            print("Table already created!", err)
+    
+    def add_user(self, nametable, login, password):
+        print(nametable,login,password)
+        self.cur.execute(f"""INSERT INTO {nametable}(login,password) VALUES (login="{login}", password="{password}")""")
+        self.con.commit()
+
+    def exit(self):
+        sys.exit()
---
+++ main.py
@@ -0,0 +1,28 @@
+from app.methods import DBases
+
+DB = DBases()
+
+if __name__ == "__main__":
+    print("Wellcome, what do you want?\n1)Create base\n2)Exit")
+    enter = int(input("Enter: "))
+    if enter == 1:
+        namedatabase = input("Enter the name of your database: ")
+        DB.connect_to_base(namedatabase)
+        print("Done! Create table? Y/n")
+        createtable = input()
+        if createtable == "Y":
+            nametable = input("Enter name of table: ")
+            DB.create_table(nametable)
+            print("Done! Add the user? Y/n")
+            adduser = input()
+            if adduser == "Y":
+               login = input("Enter login: ")
+               password = input("Enter password: ")
+               DB.add_user(nametable,login,password)
+               print("Done!")
+            else:
+                DB.exit()
+        else:
+            DB.exit()
+    if enter == 2:
+        DB.exit()
